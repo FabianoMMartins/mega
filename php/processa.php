546fd4066365a65fb9bba6b57346cf1e1344d80e
Fabiano Miguel Martins <fabianomartins_scs@yahoo.com.br>
2018-02-16 15:14:02 -0200

    Alteracoes e atualizações

--- php/processa.php
+++ php/processa.php
@@ -7,29 +7,40 @@
 
 
 //PEGO OS CAMPOS //
-    $nome          = $_POST["nome"];
-    $ddd_celular   = $_POST["ddd_celular"];
-    $celular       = $_POST["celular"];
-    $email         = $_POST["email"];
-    $ddd_telefone  = $_POST["ddd_telefone"];
-    $telefone      = $_POST["telefone"];
-    $cidade        = $_POST["cidade"];
-    $estado        = $_POST["estado"];
-    $ddd_cep       = $_POST["ddd_cep"];  
-    $interrogacao  = $_POST["interrogacao"];
-    $necessidade   = $_POST["necessidade"];
-    $mensagem      = $_POST["mensagem"];
+    $nome          = addslashes($_POST["nome"]);
+    $ddd_celular   = addslashes($_POST["ddd_celular"]);
+    $celular       = addslashes($_POST["celular"]);
+    $email         = addslashes($_POST["email"]);
+    $profissao     = addslashes($_POST["profissao"]);
+    $ddd_telefone  = addslashes($_POST["ddd_telefone"]);
+    $telefone      = addslashes($_POST["telefone"]);
+    $cidade        = addslashes($_POST["cidade"]);
+    $estado        = addslashes($_POST["estado"]);
+    $ddd_cep       = addslashes($_POST["ddd_cep"]);  
+    $interrogacao  = addslashes($_POST["interrogacao"]);
+    $necessidade   = addslashes($_POST["necessidade"]);
+    $mensagem      = addslashes($_POST["mensagem"]);
 //PEGO OS CAMPOS //
 
 
 
-    
 //INSERIR//
-    $busca  = $pdo->prepare("INSERT INTO leads(nome)VALUES(:nome)");
+    $busca  = $pdo->prepare("INSERT INTO leads(nome,ddd_celular,celular,email,profissao,ddd_telefone,telefone,cidade,estado,ddd_cep,interrogacao,necessidade,mensagem)VALUES(:nome, :ddd_celular, :celular, :email, :profissao, :ddd_telefone, :telefone, :cidade, :estado, :ddd_cep, :interrogacao, :necessidade, :mensagem)");
     $busca->bindValeu(":nome",$nome);
+    $busca->bindValeu(":ddd_celular", $ddd_celular);
+    $busca->bindValeu(":celular", $celular);
+    $busca->bindValeu(":email", $email);
+    $busca->bindValeu(":profissao", $profissao);
+    $busca->bindValeu(":ddd_telefone", $ddd_telefone);
+    $busca->bindValeu(":telefone", $telefone);
+    $busca->bindValeu(":cidade", $cidade);
+    $busca->bindValeu(":estado", $estado);
+    $busca->bindValeu(":ddd_cep", $ddd_cep);
+    $busca->bindValeu(":interrogacao", $interrogacao);
+    $busca->bindValeu(":necessidade", $necessidade);
+    $busca->bindValeu(":mensagem", $mensagem);
     $busca->execute();
 //INSERIR//
-
 
 
 
@@ -39,6 +50,7 @@ session_start();
   $_SESSION['ddd_celular']   = $ddd_celular;
   $_SESSION['celular']       = $celular;
   $_SESSION['email']         = $email;
+  $_SESSION['profissao']     = $profissao;
   $_SESSION['ddd_telefone']  = $ddd_telefone;
   $_SESSION['telefone']      = $telefone;
   $_SESSION['cidade']        = $cidade;
@@ -48,8 +60,6 @@ session_start();
   $_SESSION['necessidade']   = $necessidade;
   $_SESSION['mensagem']      = $mensagem;
 //SESSAO//
-
-
 
 
 
